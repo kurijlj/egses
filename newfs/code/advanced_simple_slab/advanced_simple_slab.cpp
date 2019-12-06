a69eae9e3d3eef183d5fffa656e7ced99dda9b9e
Ljubomir Kurij <kurijlj@gmail.com>
2019-12-06 11:36:22 +0100

    SNC Dosimetry: (WIP) Discarded lines that generate compiler errors in the AdvancedSimpleSlab code

--- newfs/code/advanced_simple_slab/advanced_simple_slab.cpp
+++ newfs/code/advanced_simple_slab/advanced_simple_slab.cpp
@@ -36,7 +36,6 @@ void AdvancedSimpleSlabCode::describeUserCode() const {
     egsSimplifyCVSKey(revision).c_str(),
     egsSimplifyCVSKey(base_rev_info).c_str());
     egsInformation("\n\n");
-    describeSimulation();
 }
 
 int AdvancedSimpleSlabCode::initGeometry() {
@@ -367,9 +366,6 @@ int AdvancedSimpleSlabCode::outputData() {
     if(!eflu->storeState(*data_out)) return 301;
     if(!gflu->storeState(*data_out)) return 302;
 
-    // Not sure if this is good way to do it!?
-    if(!np_hist->storeState(*data_out)) return 303;
-
     return 0;
 }
 
@@ -388,9 +384,6 @@ int AdvancedSimpleSlabCode::readData() {
     if(!eflu->setState(*data_in)) return 301;
     if(!gflu->setState(*data_in)) return 302;
 
-    // Not sure if this is good way to do it!?
-    if(!np_hist->setState(*data_out)) return 303;
-
     return 0;
 }
 
@@ -402,9 +395,6 @@ void AdvancedSimpleSlabCode::resetCounter() {
     score->reset();
     eflu->reset();
     gflu->reset();
-
-    // I guess I should do the same for the np_hist scoring object.
-    np_hist->reset();
 }
 
 int AdvancedSimpleSlabCode::addState(istream &data) {
@@ -425,10 +415,6 @@ int AdvancedSimpleSlabCode::addState(istream &data) {
     (*eflu) += tmp1;
     if(!tmp1.setState(data)) return 302;
     (*gflu) += tmp1;
-
-    // The same here as for resetCounter().
-    if(!tmp1.setState(data)) return 303;
-    (*np_hist) += tmp1;
 
     return 0;
 }
@@ -506,7 +492,6 @@ int AdvancedSimpleSlabCode::startNewShower() {
         score->setHistory(current_case);
         eflu->setHistory(current_case);
         gflu->setHistory(current_case);
-        np_hist->setHistory(current_case);
         last_case = current_case;
     }
     current_weight = p.wt;
